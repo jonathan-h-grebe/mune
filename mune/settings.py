6b512888e8968a930e37b5a492efd80d2604f6b4
furuya@MBP <takashi.furuya.0@gmail.com>
2020-10-18 17:12:37 +0900

    MEDIAの設定

--- mune/settings.py
+++ mune/settings.py
@@ -149,7 +149,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+MEDIA_URL = '/media/'
+MEDIA_ROOT = (
+    os.path.join(BASE_DIR, 'media')
+)
 
 # django-allauth
 #追加終わったら閉じる
