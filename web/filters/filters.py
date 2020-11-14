255c96ca41963629c1d5a4e8eec0a8dde0c15d0d
Jonathan Hill <jono-tree@hotmail.co.uk>
2020-11-14 15:32:03 +0900

    要らないファイルの削除（filters)

--- web/filters/filters.py
+++
@@ -1,9 +0,0 @@
-import django_filters
-from web.models import Item
-
-class ProductFilter(django_filters.FilterSet):
-    name = django_filters.CharFilter(lookup_expr='iexact')
-
-    class Meta:
-        model = Product
-        fields = ['area']
